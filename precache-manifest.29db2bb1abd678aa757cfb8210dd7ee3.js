dffe9bdcbfd3e7a497dcc3eab0dccde34477f8f0
Rachel Sleet <rachelsleet@gmail.com>
2019-03-19 23:30:09 +0100

    Updates

---
+++ precache-manifest.29db2bb1abd678aa757cfb8210dd7ee3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95792f95ecfb90844180",
+    "url": "/drum-machine-fcc/static/js/runtime~main.7e1d3c4f.js"
+  },
+  {
+    "revision": "7ccd00ad1315073bbafa",
+    "url": "/drum-machine-fcc/static/js/main.5971d317.chunk.js"
+  },
+  {
+    "revision": "782431a7a9fd8d41f546",
+    "url": "/drum-machine-fcc/static/js/2.8b2938bc.chunk.js"
+  },
+  {
+    "revision": "7ccd00ad1315073bbafa",
+    "url": "/drum-machine-fcc/static/css/main.841a304d.chunk.css"
+  },
+  {
+    "revision": "98b2c7a930a1f07b66d5b992c0d7d95a",
+    "url": "/drum-machine-fcc/index.html"
+  }
+];
