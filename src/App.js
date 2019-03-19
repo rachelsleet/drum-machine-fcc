918f96f24440c822e66e6d91997802d6f2b327fb
Rachel Sleet <rachelsleet@gmail.com>
2019-03-19 23:24:54 +0100

    stying done

--- src/App.js
+++ src/App.js
@@ -4,47 +4,47 @@ import './App.css';
 const soundBank = [{
   keyCode: 81,
   keyTrigger: 'Q',
-  id: 'Chord-1',
+  id: 'Chord-1.',
   url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
 }, {
   keyCode: 87,
   keyTrigger: 'W',
-  id: 'Chord-2',
+  id: 'Chord-2.',
   url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
 }, {
   keyCode: 69,
   keyTrigger: 'E',
-  id: 'Chord-3',
+  id: 'Chord-3.',
   url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
 }, {
   keyCode: 65,
   keyTrigger: 'A',
-  id: 'Shaker',
+  id: 'Shaker.',
   url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'
 }, {
   keyCode: 83,
   keyTrigger: 'S',
-  id: 'Open-HH',
+  id: 'Open-HH.',
   url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'
 }, {
   keyCode: 68,
   keyTrigger: 'D',
-  id: 'Closed-HH',
+  id: 'Closed-HH.',
   url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
 }, {
   keyCode: 90,
   keyTrigger: 'Z',
-  id: 'Punchy-Kick',
+  id: 'Punchy-Kick.',
   url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
 }, {
   keyCode: 88,
   keyTrigger: 'X',
-  id: 'Side-Stick',
+  id: 'Side-Stick.',
   url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'
 }, {
   keyCode: 67,
   keyTrigger: 'C',
-  id: 'Snare',
+  id: 'Snare.',
   url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'
 }];
 
@@ -52,7 +52,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      soundToDisplay: 'Bust a beat'
+      soundToDisplay: 'Play the keys..'
     }
     this.displayKeyDown = this.displayKeyDown.bind(this);
     this.updateDisplay = this.updateDisplay.bind(this);
